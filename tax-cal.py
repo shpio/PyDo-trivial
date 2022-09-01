275d44d73581e39d782bc899aa499fc56776ab78
shpio <112623455+shpio@users.noreply.github.com>
2022-09-01 16:32:08 +0200

    Update tax-cal.py

--- tax-cal.py
+++ tax-cal.py
@@ -1,20 +1,39 @@
-# https://edube.org/learn/pe-1/lab-essentials-of-the-if-else-statement-3
-
+## https://edube.org/learn/pe-1/lab-essentials-of-the-if-else-statement-3
+#
+#income = float(input("Enter the annual income: "))
+## relief is a PIT - 18% from income
+#
+#if income <= 85528:
+#    tax = round(income / 100 * 18 - 556.02,0)
+#    if tax <= 0:   
+#        print("The tax is: 0.0 thalers")
+#    else:
+#        print("The tax is:", tax, "thalers")
+#
+#if income > 85528:
+#    surplus = income - 85528
+## tax for income above 85528 is sum of 14839.2 and 32% of surplus
+#    tax = round(14839.02 + (surplus / 100 * 32),0)
+#    if tax <= 0:
+#        print("The tax is: 0.0 thalers")
+#    else:        
+#        print("The tax is:", tax, "thalers" )
+#
+# cleanier version
 income = float(input("Enter the annual income: "))
 # relief is a PIT - 18% from income
 
 if income <= 85528:
     tax = round(income / 100 * 18 - 556.02,0)
-    if tax <= 0:   
-        print("The tax is: 0.0 thalers")
-    else:
-        print("The tax is:", tax, "thalers")
 
 if income > 85528:
     surplus = income - 85528
 # tax for income above 85528 is sum of 14839.2 and 32% of surplus
     tax = round(14839.02 + (surplus / 100 * 32),0)
-    if tax <= 0:
-        print("The tax is: 0.0 thalers")
-    else:        
-        print("The tax is:", tax, "thalers" )
+
+if tax <= 0:
+    print("The tax is: 0.0 thalers")
+else:    
+    print("The tax is:", tax, "thalers" )
+
+
