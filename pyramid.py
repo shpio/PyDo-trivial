d43b93171d73c36fec1673222a4a00d6c04fe049
shpio <shpionick@protonmail.com>
2022-09-05 15:58:00 +0200

    pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,72 @@
+blocks = int(input("Enter the number of blocks: "))
+height = 0
+while blocks > height:
+# a = 1
+# b = a + 1
+    height = height + 1
+    blocks = blocks - height
+print("The height of the pyramid:", height)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# blocks = int(input("Enter the number of blocks: "))
+# blocks_ = blocks
+# dif_step = 1
+# while  blocks != 0 :
+
+
+#     blocks = blocks_ - dif_step
+#     dif_step += 1
+#
+#     print("Blocks left", blocks)
+# #print("The height of the pyramid:", height)
+
+
+# blocks = int(input("Enter the number of blocks: "))
+# blocks_ = blocks
+# dif_step = 1
+# while blocks != 0:
+#     blocks = blocks_ - dif_step
+#     dif_step += 1
+#
+#     print("Blocks left", blocks)
+# # print("The height of the pyramid:", height)
+#
+
+# blocks = int(input("Enter the number of blocks: "))
+# a = 1
+# while
+#     b = a + 1
+#     c = b + 1
+#     d = c + 1
+#
+#
+# hight 1  = blocks 1
+# height 2 = blocks 3
+# height 3 = blocks 6
+# 1 ** 5 + 1 ** 4 + 1 ** 3
+#
+
+
