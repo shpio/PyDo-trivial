65a8e230fe6565b6379c2357ba0ffcdaedb26507
shpio <shpionick@protonmail.com>
2022-09-05 21:52:29 +0200

    add c0.py

---
+++ practice.py
@@ -0,0 +1,50 @@
+# # Create a for loop that counts from 0 to 10, and prints odd numbers to the screen.
+# for i in range(0, 11):
+#     if i % 2 != 0:
+#         print(i)
+
+
+# x = 1
+# while x < 11:
+#     x = x + 1
+#     if x % 2 != 0:
+#         print(x)
+#     pass
+
+# x = 1
+# while x < 11:
+#     if x % 2 != 0:
+#         print(x)
+#     x += 1
+
+
+# for ch in "john.smith@pythoninstitute.org":
+#     if ch == "@":
+#         break
+#     print(ch, end="")
+
+# for digit in "0165031806510":
+#     if digit == "0":
+#         print("x", end="")
+#         continue
+#     print(digit, end="")
+
+# n = 3
+# while n > 0:
+#     m = n + 1
+#     print("m", m)
+#     n -= 1
+# else:
+#     print("n", n)
+
+# n = range(4)
+#
+# for num in n:
+#     print(num - 1)
+# else:
+#     print(num)
+
+
+#
+# for i in range(0, 6, 3):
+#     print(i)
