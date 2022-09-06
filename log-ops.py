6947952a9cb6c389071c253c99b3933664c1cd91
shpio <shpionick@protonmail.com>
2022-09-07 00:35:14 +0200

    some bit mapping exercise

---
+++ log-ops.py
@@ -0,0 +1,30 @@
+# i = 1
+# j = not not i
+# print(j)
+
+# i = 15
+# j = 22
+
+# var = 17
+# var_right = var >> 1
+# var_left = var << 2
+# print(var, var_left, var_right)
+
+# x = 1
+# y = 0
+#
+# z = ((x == y) and (x == y)) or not(x == y)
+# print(not(z))
+
+
+# x = 4 # 0100
+# y = 1 # 0001
+#
+# a = x & y # 0000
+# b = x | y # 0101 = 5
+# c = ~x  # tricky! = 1011 = ?
+# d = x ^ 5 # 1
+# e = x >> 2 # 1
+# f = x << 2 # 16
+#
+# print(a, b, c, d, e, f)
