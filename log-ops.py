fb9a104505c2afa4f786b370fa48e806f082460d
shpio <shpionick@protonmail.com>
2022-09-07 18:32:46 +0200

    update list

--- log-ops.py
+++ log-ops.py
@@ -172,3 +172,35 @@
 
 # lab
 
+# step 1
+beatles = []
+print("Step 1:", beatles)
+
+# step 2
+beatles.append("John lennon")
+beatles.append('Paul McCartney')
+beatles.append('George Harrison')
+
+print("Step 2:", beatles)
+
+# step 3
+for i in 'Stu Sutcliffe', 'Pete Best':
+    beatles.append(i)
+print("Step 3:", beatles)
+
+beatles.index('Pete Best', 1)
+
+# step 4
+# del beatles[3]
+# del beatles[3]
+## or
+del beatles[-2]
+del beatles[-1]
+print("Step 4:", beatles)
+
+# step 5
+beatles.insert(0, 'Ringo Starr')
+print("Step 5:", beatles)
+
+# testing list legth
+print("The Fab", len(beatles))
