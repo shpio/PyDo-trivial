13edab53eb0a9ee6ccd530c8b23ae8cac2541f34
shpio <shpionick@protonmail.com>
2022-09-12 01:57:21 +0200

    update list

--- sort.py
+++ sort.py
@@ -144,7 +144,48 @@
 # my_list = [3, 11, 5, 1, 9, 7, 15, 13, 17]
 # largest = my_list[0]
 #
-# for i in range(1, len(my_list)):
+# for i in range(len(my_list)):
 #     if my_list[i] > largest:
 #         largest = my_list[i]
 # print(largest)
+
+# my_list = [17, 3, 11, 5, 1, 9, 7, 15, 13]
+# largest = my_list[0]
+#
+# for i in my_list:
+#     if i > largest:
+#         largest = i
+# print(largest)
+
+
+#slice to save comp power
+# my_list = [3, 11, 5, 1, 9, 7, 15, 13, 17]
+# largest = my_list[0]
+#
+# for i in my_list[1:]:
+#     if i > largest:
+#         largest = i
+#
+# print(largest)
+
+# works
+# my_list = [3, 11, 5, 1, 9, 7, 15, 13, 17]
+# largest = my_list[0]
+#
+# for i in my_list:
+#     if i > largest:
+#         largest = i
+#
+# print(largest)
+
+# fixed
+# my_list = [3, 11, 5, 1, 9, 7, 15, 13, 17]
+# largest = my_list[0]
+#
+# # for i in range(1, len(my_list)):
+# for i in my_list:
+#     if i > largest:
+#         largest = i
+#
+# print(largest)
+
