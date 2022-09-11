5ac343c189302098bf16e6bb6071429566eece20
shpio <shpionick@protonmail.com>
2022-09-12 01:17:30 +0200

    update list

--- sort.py
+++ sort.py
@@ -141,3 +141,10 @@
 # if to_find in my_list:
 #     print(to_find)
 
+# my_list = [3, 11, 5, 1, 9, 7, 15, 13, 17]
+# largest = my_list[0]
+#
+# for i in range(1, len(my_list)):
+#     if my_list[i] > largest:
+#         largest = my_list[i]
+# print(largest)
