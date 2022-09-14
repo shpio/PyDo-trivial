57b6c6af28e641d13158ed0398d34de3778d751d
shpio <shpionick@protonmail.com>
2022-09-15 00:04:22 +0200

    update list

--- sort.py
+++ sort.py
@@ -216,13 +216,69 @@
 # else:
 #     print("absent")
 
-drawn = [5, 11, 9, 42, 3, 49]
-bets = [3, 7, 11, 42, 34, 49]
-hits = 0
 
-for i in bets:
-    if i in drawn:
-        hits += 1
-        var = print(i)
-        continue
-print("You guess righ 4 numbers", hits)
+
+
+# drawn = [5, 11, 9, 42, 3, 49]
+# bets = [3, 7, 11, 42, 34, 49]
+# hits = 0
+
+# for i in bets:
+#     if i in drawn:
+#         hits += 1
+#         var = print(i)
+#         continue
+# print("You guess righ 4 numbers", hits)
+
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# to_find = 5
+# found = False
+#
+# for i in range(len(my_list)):
+#     found = my_list[i] == to_find
+#     if found:
+#         break
+#
+# if found:
+#     print("Element found at index", i)
+# else:
+#     print("absent")
+
+my_list = [1, 2, 4, 4, 1, 4, 2, 6, 2, 9]
+
+# new_list = my_list[:]
+# print(new_list)
+
+for i in my_list:
+    if my_list[i] == my_list[i]:
+        del my_list[i]
+
+
+# print("The list with unique elements only:")
+print(my_list)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
