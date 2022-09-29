0f5a0cb689bc71e89ecdcfaff75d9cbf5a15ce97
shpio <shpionick@protonmail.com>
2022-09-29 13:56:20 +0200

    add function is_year_leap

---
+++ soup/fic-cal-man.py
@@ -0,0 +1,13 @@
+czk_net = 83000
+czk_gro = 110000
+usd_net = czk_net * 0.039
+usd_gro = czk_gro * 0.039
+eur_net = czk_net * 0.042
+eur_gro = czk_gro * 0.042
+nok_net = czk_net * 0.42
+nok_gro = czk_gro * 0.42
+dkk_net = czk_net * 0.30
+dkk_gro = czk_gro * 0.30
+
+#print("usd_net is ", usd_net, "usd_gro is ", usd_gro, "eur_net is ", eur_net, "eur_gro is " ,eur_gro, "nok_net is " ,nok_net, "nok_gro is ", nok_gro, "dkk_net is " ,dkk_net, "dkk_gro is " ,dkk_gro, sep="\n")
+print("dkk_gro_y", dkk_gro * 12)
