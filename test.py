d20d291387af96167927a2a2933cdc44d8d0ccad
shpio <shpionick@protonmail.com>
2022-09-30 00:08:53 +0200

    add function days_in_month and start func day_of_year

--- test.py
+++ test.py
@@ -3,26 +3,24 @@ def is_year_leap(year):
     print(leap_year)
     return leap_year
 
+
 def days_in_month(year, month):
     thirty = [ 4, 6, 9, 11 ]
     thirtyone = [ 1, 3, 5, 7, 8, 10, 12 ]
     leap = is_year_leap(year) == True
 
     if month in thirtyone:
-        print(31)
+        # print(31)
         return 31
     if month in thirty:
-        print(30)
+        # print(30)
         return 30
     if month == 2 and leap:
-        print(29)
+        # print(29)
         return 29
     if month == 2 and not leap:
-        print(28)
+        # print(28)
         return 28
 
 def day_of_year(year, month, day):
-
-    day = days_in_month(year, month)
-
-print(day_of_year(2000, 12, 31))
+    for day in range (0, month)
